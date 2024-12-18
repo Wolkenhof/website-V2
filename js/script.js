e736c5a7ee6fb64c9244bea1b99ca37f55a559b7
Adrian <70568660+wsoePhantom@users.noreply.github.com>
2024-12-18 12:37:53 +0000

    [FTR] partner sites

--- js/script.js
+++ js/script.js
@@ -1140,3 +1140,42 @@ function closeElement() {
 	}, 100);
 }
 
+
+
+
+$(document).ready(function () {
+	// Configure/customize these variables.
+	var showChar = 0;  // How many characters are shown by default
+	var ellipsestext = "...";
+	var moretext = "Mehr erfahren >";
+	var lesstext = "Weniger zeigen";
+
+
+	$('.more').each(function () {
+		var content = $(this).html();
+
+		if (content.length > showChar) {
+
+			var c = content.substr(0, showChar);
+			var h = content.substr(showChar, content.length - showChar);
+
+			var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+			$(this).html(html);
+		}
+
+	});
+
+	$(".morelink").click(function () {
+		if ($(this).hasClass("less")) {
+			$(this).removeClass("less");
+			$(this).html(moretext);
+		} else {
+			$(this).addClass("less");
+			$(this).html(lesstext);
+		}
+		$(this).parent().prev().toggle();
+		$(this).prev().toggle();
+		return false;
+	});
+});
